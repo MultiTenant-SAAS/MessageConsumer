94575bab0d002e5b08ff0e708a451e8fa43c3fb5
sovon singha <sovonsingha9@gmail.com>
2025-05-02 09:14:52 +0530

    2nd may

--- src/main/java/com/plantapps/MessageConsumer/MessageConsumerApplication.java
+++ src/main/java/com/plantapps/MessageConsumer/MessageConsumerApplication.java
@@ -1,13 +1,29 @@
 package com.plantapps.MessageConsumer;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.plantapps.MessageConsumer.kafka.CustomListner;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class MessageConsumerApplication {
 
+	@Autowired
+	private CustomListner custistener;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MessageConsumerApplication.class, args);
 	}
+	
+//	@PostConstruct
+//    public void init() {
+//        List<String> tenantIds = List.of("1", "2"); // or load from DB
+//        tenantIds.forEach(custistener::registerTenantListener);
+//    }
 
 }
--- src/main/java/com/plantapps/MessageConsumer/kafka/KkafkaMessageConsumer.java
+++ src/main/java/com/plantapps/MessageConsumer/kafka/KkafkaMessageConsumer.java
@@ -9,24 +9,39 @@ import com.plantapps.MessageConsumer.model.Message;
 @Service
 public class KkafkaMessageConsumer
 {
-//	@KafkaListener(topics = "site_1", groupId = "message_1")
-//	public void mesageConsumer(Message message)
-//	{
-//		System.out.println("the message received is : "+message);
-//	}
+	@KafkaListener(topics = "site_1", groupId = "message_1")
+	public void mesageConsumer(Message message)
+	{
+		System.out.println("the message received is : "+message);
+	}
 	
 	@KafkaListener(topics = "site_1", groupId = "message_1")
-	public void mesageConsumer(ConsumerRecord<String, Message> record)
+	public void mesageConsumer1(ConsumerRecord<String, Message> record)
 	{
 		Message message = record.value();
 		String id = new String(record.headers().lastHeader("tenantId").value());
 		if("tenant-1".equals(id))
 		{
-			System.out.println("the message received from tenant-1 is : "+message.getData());
+			System.out.println("the message received from site_1 tenant-1 is : "+message.getData());
 		}
 		if("tenant-2".equals(id))
 		{
-			System.out.println("the message received from tenant-2 is : "+message.getData());
+			System.out.println("the message received from site_1 tenant-2 is : "+message.getData());
+		}
+	}
+	
+	@KafkaListener(topics = "site_2", groupId = "message_1")
+	public void mesageConsumer2(ConsumerRecord<String, Message> record)
+	{
+		Message message = record.value();
+		String id = new String(record.headers().lastHeader("tenantId").value());
+		if("tenant-1".equals(id))
+		{
+			System.out.println("the message received from site_2 tenant-1 is : "+message.getData());
+		}
+		if("tenant-2".equals(id))
+		{
+			System.out.println("the message received from site_2 tenant-2 is : "+message.getData());
 		}
 	}
 }
