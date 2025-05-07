84597082fd263f3e0c69e8046553c3146baf362e
sovon singha <sovonsingha9@gmail.com>
2025-05-07 16:56:28 +0530

    7th may

--- src/main/java/com/plantapps/MessageConsumer/kafka/KkafkaMessageConsumer.java
+++ src/main/java/com/plantapps/MessageConsumer/kafka/KkafkaMessageConsumer.java
@@ -1,7 +1,9 @@
 package com.plantapps.MessageConsumer.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import com.plantapps.MessageConsumer.model.Message;
@@ -9,6 +11,8 @@ import com.plantapps.MessageConsumer.model.Message;
 @Service
 public class KkafkaMessageConsumer
 {
+	@Autowired
+	private SimpMessagingTemplate messagingTemplate;
 	
 	@KafkaListener(topics = "site_1", groupId = "message_1")
 	public void mesageConsumer1(ConsumerRecord<String, Message> record)
@@ -16,6 +20,7 @@ public class KkafkaMessageConsumer
 		Message message = record.value();
 		//String id = new String(record.headers().lastHeader("tenantId").value());
 		System.out.println("the message received from site_1 tenant-1 is : " + message.getData());
+		messagingTemplate.convertAndSend("/topic/site_1", message);
 	}
 	
 	@KafkaListener(topics = "site_2", groupId = "message_1")
@@ -25,5 +30,6 @@ public class KkafkaMessageConsumer
 		//String id = new String(record.headers().lastHeader("tenantId").value());
 
 		System.out.println("the message received from site_2 tenant-2 is : " + message.getData());
+		messagingTemplate.convertAndSend("/topic/site_2", message);
 	}
 }
